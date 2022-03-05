f6104388a146c2a426a36d5514331596235760f7
Ying <97061798+yingmo55@users.noreply.github.com>
2022-03-05 11:08:27 -0500

    Add function to generate nearby 3x3 grid

--- src/Utils/sudokuGenerator/sudokuGenerator.js
+++ src/Utils/sudokuGenerator/sudokuGenerator.js
@@ -1,6 +1,9 @@
 const sudokuGenerator = {
     rows: 'ABCDEFGHI',
     columns: '123456789',
+    rowGrid: [ ['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I'] ],
+    columnGrid: [ ['1','2','3'],['4','5','6'],['7','8','9'] ],
+
 
     initializeGrid() {
         const possibleAnswer = Array.from(this.columns);
@@ -23,10 +26,32 @@ const sudokuGenerator = {
         sameRow = sameRow.filter(element => element !== coord);
         let sameColumn = Array.from(this.rows).map(element => element + coord[1]);
         sameColumn = sameColumn.filter(element => element !== coord);
-        const sameGrid = []
-        const unit = [].concat(sameRow, sameColumn, ['B2', 'C2', 'B3', 'C3'])
+
+
+        // get the squares within the same grid
+        let sameGridRow = this.rowGrid.filter(arr => arr.includes(coord[0]))[0]
+        sameGridRow = sameGridRow.filter(element=>element !== coord[0])
+
+        let sameGridColumn = this.columnGrid.filter(arr => arr.includes(coord[1]))[0]
+        sameGridColumn = sameGridColumn.filter(element=>element !== coord[1])
+
+        const nearbyGrid = () => {
+            let nearby = [];
+            for (let i=0; i < sameGridColumn.length; i++){ // rows
+                for (let x=0; x <  sameGridRow.length; x++){ // columns
+                    const coord = sameGridRow[x] + sameGridColumn[i];
+                    nearby.push(coord);
+                }
+            }
+            return nearby;
+        }
+
+        const nearby = nearbyGrid();
+        const unit = [].concat(sameRow, sameColumn, nearby)
         return unit;
     }
 }
 
 module.exports = sudokuGenerator;
+
+sudokuGenerator.initializeGrid()
