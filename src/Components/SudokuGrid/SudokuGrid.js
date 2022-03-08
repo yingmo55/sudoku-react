e8cc2d52fcea20a32acaa1f08e09ddf759a41ffb
evergreenlilac <sireteller@gmail.com>
2022-03-09 00:24:31 +0200

    Fix issue #1. The effort to keep the sudoku grid input type 'number' ultimately isn't worth it, using type 'text' and regex is cleaner, though it isn't ideal for accessibility.

--- src/Components/SudokuGrid/SudokuGrid.js
+++ src/Components/SudokuGrid/SudokuGrid.js
@@ -4,17 +4,18 @@ export function SudokuGrid() {
 
     const blockFalseInput = (e) => {
         const inputValue = e.target.value;
-        //Prevent user from inserting a numerical value longer than 1 digit and replace the old value with the most recently entered number
-        if (parseInt(inputValue) > 9) {
-            e.target.value = inputValue[1];
-        }
-        //Prevent user from entering any zeros and non-numbers, default the value to 1
-        if (inputValue.includes('0') || !inputValue) {
-            e.target.value = '1';
+        const regExp = /^[1-9]$/;
+        if (!inputValue.match(regExp)) {
+            e.preventDefault();
+            if (inputValue[0].match(regExp)) {
+                e.target.value = inputValue[0];
+            } else {
+                e.target.value = '';
+            }
         }
     }
 
-    const inputBox = <input type='number' name='number' inputMode='numeric' min='1' max='9' onChange={blockFalseInput} onKeyPress={blockFalseInput} />;
+    const inputBox = <input type='text' name='number' inputMode='numeric' onChange={blockFalseInput} />;
     const threeByThree = (<div className='three-by-three'>
             {inputBox}{inputBox}{inputBox}<br/>
             {inputBox}{inputBox}{inputBox}<br/>
